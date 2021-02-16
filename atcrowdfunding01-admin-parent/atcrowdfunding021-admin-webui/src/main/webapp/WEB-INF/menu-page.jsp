5ec7664f88799d0345c5a91c152bf997730709fb
FLY <610841614@qq.com>
2021-02-16 23:58:45 +0800

    菜单维护：后端构造menu树结构

---
+++ atcrowdfunding01-admin-parent/atcrowdfunding021-admin-webui/src/main/webapp/WEB-INF/menu-page.jsp
@@ -0,0 +1,24 @@
+<%@ page contentType="text/html;charset=UTF-8" pageEncoding="UTF-8" isELIgnored="false" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+
+<!DOCTYPE html>
+<html lang="zh-CN">
+<%@ include file="/WEB-INF/include-head.jsp" %>
+<body>
+<%@ include file="/WEB-INF/include-nav.jsp" %>
+<div class="container-fluid">
+    <div class="row">
+        <%@ include file="/WEB-INF/include-sidebar.jsp" %>
+        <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+
+            <div class="panel panel-default">
+                <div class="panel-heading"><i class="glyphicon glyphicon-th-list"></i> 权限菜单列表 <div style="float:right;cursor:pointer;" data-toggle="modal" data-target="#myModal"><i class="glyphicon glyphicon-question-sign"></i></div></div>
+                <div class="panel-body">
+                    <ul id="treeDemo" class="ztree"></ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
---
+++ atcrowdfunding01-admin-parent/atcrowdfunding03-admin-component/src/main/java/com/atguigu/crowd/mvc/handler/MenuHandler.java
@@ -0,0 +1,86 @@
+package com.atguigu.crowd.mvc.handler;
+
+import com.atguigu.crowd.entity.Menu;
+import com.atguigu.crowd.service.api.MenuService;
+import com.atguigu.crowd.util.ResultEntity;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Objects;
+
+@Controller
+public class MenuHandler {
+
+    private Logger logger = LoggerFactory.getLogger(MenuHandler.class);
+
+    @Autowired
+    private MenuService menuService;
+
+    /**
+     * 使用双重嵌套循环构造树结构，时间复杂度太高，T(n)=O(n^2)
+     */
+    @RequestMapping("/menu/get/whole/tree/old.json")
+    @ResponseBody
+    public ResultEntity<Menu> getWholeTreeOld() {
+        // 查出所有menu对象
+        List<Menu> menuList = menuService.getAll();
+        // 记录根节点
+        Menu root = null;
+        // 遍历menuList
+        for (Menu menu : menuList) {
+            Integer pid = menu.getPid();
+            if (pid == null) {
+                // 无父节点，则为根节点
+                root = menu;
+                continue;
+            }
+            // 有父节点，尝试寻找父节点，建立父子关系
+            for (Menu maybeFather : menuList) {
+                if (Objects.equals(pid, maybeFather.getId())) {
+                    // 找到父节点
+                    maybeFather.getChildren().add(menu);
+                    // 一个子节点只有一个父节点，找到即可跳出循环
+                    break;
+                }
+            }
+        }
+        return ResultEntity.successWithData(root);
+    }
+
+    /**
+     * 在第一次遍历时用一个hashMap记录id和menu的对应关系，降低时间复杂度（使用hashMap空间换时间），T(n)=O(n)
+     */
+    @RequestMapping("/menu/get/whole/tree.json")
+    @ResponseBody
+    public ResultEntity<Menu> getWholeTree() {
+        // 查出所有menu对象
+        List<Menu> menuList = menuService.getAll();
+        // 记录根节点
+        Menu root = null;
+        // 用一个map记录id和menu的对应关系
+        HashMap<Integer, Menu> menuMap = new HashMap<>();
+        // 第一次遍历，用于记录menu的id与自身的对应关系，方便第二次循环直接通过pid获取menu本身
+        for (Menu menu : menuList) {
+            menuMap.put(menu.getId(), menu);
+        }
+        // 第二次遍历，查找根节点，以及建立父子关系
+        for (Menu menu : menuList) {
+            Integer pid = menu.getPid();
+            if (pid == null) {
+                // 无父节点，则为根节点
+                root = menu;
+                continue;
+            }
+            // 有父节点，则建立父子关系
+            Menu father = menuMap.get(pid);
+            father.getChildren().add(menu);
+        }
+        return ResultEntity.successWithData(root);
+    }
+}
